a060e26d93701e855092f2837dcee272424877ae
Clément BALLOT <clement.ballot38@gmail.com>
2022-03-22 19:57:05 +0100

    switched to scss

--- PTab/src/app/app.component.ts
+++ PTab/src/app/app.component.ts
@@ -10,7 +10,7 @@ declare global {
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
--- PTab/src/app/home/home.component.ts
+++ PTab/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Globales, SearchVerticalPosition, SearchHorizontalPosition, Entry } fro
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
-	styleUrls: ['./home.component.css']
+	styleUrls: ['./home.component.scss']
 })
 
 
